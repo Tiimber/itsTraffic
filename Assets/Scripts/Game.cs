ab53b2a16fe040c2700c96a7255b5f6765b95303
Robbin Tapper <robbin.tapper@mobenga.com>
2015-04-14 13:00:53 +0200

    XML

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Xml;
+
+public class Game : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (loadXML ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private IEnumerator loadXML () {
+		string configFile = "file://" + Application.streamingAssetsPath + "/testmap01.osm"; 
+		WWW www = new WWW (configFile);
+		
+		yield return www;
+
+		XmlDocument xmlDoc = new XmlDocument();
+		xmlDoc.LoadXml(www.data);
+
+		Debug.Log (xmlDoc.SelectNodes("osm/node"));
+	}
+}

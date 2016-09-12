f7050e45583016c2a3f8466d7b1c5ad2c7194ae7
Robbin Tapper <robbin.tapper@mobenga.com>
2016-09-12 11:43:29 +0200

    Better restart states and memory of settings

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		loadPlayerPrefs();
+	}
+
+    public void loadPlayerPrefs() {
+        string storedKeysStr = PlayerPrefs.GetString("Menu:storedKeys");
+       	if (storedKeysStr != null && storedKeysStr != "") {
+            // Get all MenuValue items
+            MenuValue[] menuValueObjects = Resources.FindObjectsOfTypeAll<MenuValue>();
+            Dictionary<string, MenuValue> menuValueObjectsWithKeys = new Dictionary<string, MenuValue>();
+            foreach (MenuValue menuValueObject in menuValueObjects) {
+                if (!menuValueObjectsWithKeys.ContainsKey(menuValueObject.key)) {
+                    menuValueObjectsWithKeys.Add(menuValueObject.key, menuValueObject);
+                }
+            }
+
+	        string[] storedKeys = storedKeysStr.Split(',');
+            foreach (string storedKey in storedKeys) {
+                // Find MenuValue object and set the stored value in it
+                if (menuValueObjectsWithKeys.ContainsKey(storedKey)) {
+                    MenuValue menuValueObject = menuValueObjectsWithKeys[storedKey];
+                    object value;
+                    if (menuValueObject.formatType == MenuValue.FORMAT_TYPES.Float) {
+                        value = PlayerPrefs.GetFloat(storedKey);
+                    } else if (menuValueObject.formatType == MenuValue.FORMAT_TYPES.Integer) {
+                        value = PlayerPrefs.GetInt(storedKey);
+                    } else if (menuValueObject.formatType == MenuValue.FORMAT_TYPES.String) {
+                        value = PlayerPrefs.GetString(storedKey);
+                    } else {
+                        continue;
+                    }
+                    menuValueObject.setValue(value);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/MenuValue.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuValue : MonoBehaviour {
+
+    public enum FORMAT_TYPES {
+        String,
+        Float,
+        Integer
+    };
+
+    public enum INPUT_TYPES {
+        Slider,
+        Text
+    };
+
+    public FORMAT_TYPES formatType;
+    public INPUT_TYPES inputType;
+
+    public string key;
+
+    public void setValue(object value) {
+        switch (formatType) {
+            case FORMAT_TYPES.String:
+                setStringValue((string) value);
+            	break;
+            case FORMAT_TYPES.Float:
+                setFloatValue((float) value);
+    	        break;
+            case FORMAT_TYPES.Integer:
+                setIntValue((int) value);
+	            break;
+        }
+    }
+
+    private void setStringValue(string value) {
+        if (inputType == INPUT_TYPES.Text) {
+            Text textObject = GetComponent<Text>();
+            textObject.text = value;
+        }
+        // TODO - More?
+    }
+
+    private void setFloatValue(float value) {
+        switch (inputType) {
+            case INPUT_TYPES.Slider:
+                Slider slider = GetComponent<Slider> ();
+                slider.value = value;
+                break;
+        }
+    }
+
+    private void setIntValue(int value) {
+        if (inputType == INPUT_TYPES.Slider) {
+            setFloatValue((float)value);
+        }
+        // TODO - More?
+    }
+}
